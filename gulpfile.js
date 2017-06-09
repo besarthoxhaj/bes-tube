31953731a3bb780c10f2bbe19b0ea405bd9cf197
besart <besartshyti@gmail.com>
2017-06-09 22:49:23 +0100

    [housekeeping] remove unused sass-docs

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@
  */
 
 const gulp = require('gulp');
-const SassDocs = require('./internals/gulp/sass-docs');
 const SvgSymbols = require('./internals/gulp/svg-symbols');
 const UpdateHtml = require('./internals/gulp/update-html');
 
@@ -13,18 +12,15 @@ const paths = {
   symbols: './assets/svg-symbols.html',
 };
 
-gulp.task('sass-docs', SassDocs({source: paths.sass}));
 gulp.task('svg-symbols', SvgSymbols({source: paths.svgs}));
 gulp.task('update-html', UpdateHtml({}));
 
 gulp.task('watch', () => {
-  gulp.watch(paths.sass, ['sass-docs']);
   gulp.watch(paths.svgs, ['svg-symbols']);
   gulp.watch(paths.symbols, ['update-html']);
 });
 
 gulp.task('default', [
-  'sass-docs',
   'svg-symbols',
   'update-html',
   'watch',
